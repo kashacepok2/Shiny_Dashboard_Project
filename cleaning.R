b085f3a30048ec64e8b5e360b02af328208170f6
euanelrick <euanelrick@gmail.com>
2023-03-20 17:03:58 +0000

    More plots cleaning etc

--- cleaning.R
+++ cleaning.R
@@ -15,7 +15,13 @@ clean_location<- function(table) {
 
 bot_sex_clean <- clean_location(bot_sex)
 
-bot_simd_clean <- clean_location(bot_simd)
+bot_simd_clean <- clean_location(bot_simd) %>% 
+  mutate(simd = as.factor(simd)) %>% 
+  mutate(lockdown = case_when(
+    quarter %in% c("2017Q3", "2017Q4", "2018Q1", "2018Q2", "2018Q3", "2018Q4",
+                   "2019Q1", "2019Q2", "2019Q3", "2019Q4", "2020Q1") ~ "pre",
+    TRUE ~ "post"
+  ))
 
 
 
