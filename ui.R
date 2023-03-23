8c4473b225c969f0295a5e3d0fb668b5aa011dfb
Ellen-94 <122041087+Ellen-94@users.noreply.github.com>
2023-03-23 12:25:22 +0000

    design changes

--- ui.R
+++ ui.R
@@ -6,8 +6,8 @@ ui <- fluidPage(
       h4("A&E Activity"), 
              
              fluidRow(
+               column(width =6,   
                
-               HTML("<br><br><br>"),
                
                HTML("<h3><b> Yearly </b> </h3>"),
                
@@ -20,31 +20,37 @@ ui <- fluidPage(
                    dataTableOutput("table_output_2")
                  )
                )
-             ),
-             
-             fluidRow(
-               
-               HTML("<br><br><br>"),
+               ), 
+      
+             column(width = 6, 
+                    
                
                HTML("<h3> <b> Seasonal </b> </h3>"),
                
+               column(width= 2, 
+               
                mainPanel(
-                 radioButtons("measure_2", "", choices = c( "Patients attending" = "number_of_attendances_aggregate", "Patients meeting care target" = "number_meeting_target_aggregate")),
-                 plotOutput("a_e_plot"),
+                 radioButtons("measure_2", "", choices = c( "Patients attending" = "number_of_attendances_aggregate", "Patients meeting care target" = "number_meeting_target_aggregate")))),
+                column(width = 10, plotOutput("a_e_plot"),
                  dataTableOutput("table_output")
                )
              ),
              
+      tags$br(), 
+      tags$hr(), 
+      tags$br(),
       
       fluidRow(
+        
         mainPanel(
           plotOutput("graph_output_ae")
         )
       )
             
-    ), 
+    )
+    ),
      
-     tabPanel("Map", 
+     tabPanel(h4("Map"), 
               fluidRow(
                 mainPanel(
                   leafletOutput("map_output_ae")
@@ -66,8 +72,7 @@ ui <- fluidPage(
       h4("Hospital Activity"), 
              
              fluidRow(
-               
-               HTML("<br><br><br>"),
+               column(width = 6, 
                
                HTML("<h3> <b> Yearly for Planned and Emergency Patients </b> </h3>"),
                
@@ -82,23 +87,27 @@ ui <- fluidPage(
                )
              ),
              
-             fluidRow(
-               HTML("<br><br><br>"),
-               
+             column(width = 6, 
+
                HTML("<h3> <b> Seasonal for Planned and Emergency Patients </b> </h3>"),
+               
+               column(width = 4, 
                
                mainPanel(
                  radioButtons("period", "", choices = c("Pandemic" = "Pandemic","Prior to pandemic" = "Pre-pandemic")),
-                 radioButtons("mes_2", "", choices = c("Total average episodes" = "episodes","Total average length of stay" = "length_of_stay")),
-                 plotOutput("season_p"),
+                 radioButtons("mes_2", "", choices = c("Total average episodes" = "episodes","Total average length of stay" = "length_of_stay")))),
+                 column(width = 8, plotOutput("season_p"),
                  dataTableOutput("table_2")
                )
              ),
              
+             tags$br(), 
+             tags$hr(), 
+             tags$br(),
+             
              fluidRow(
-               HTML("<br><br><br>"),
-               
-               HTML("<h3> <b> Intensive Care Medicine Before and During the Pandemic </b> </h3>"),
+
+               HTML("<h3> <b>   Intensive Care Medicine Before and During the Pandemic </b> </h3>"),
                
                mainPanel(
                  #selectInput("period_2", "Before or During", choices = c("Pandemic" = "Pandemic","Before Pandemic" = "Pre-pandemic")),
@@ -107,14 +116,15 @@ ui <- fluidPage(
                  dataTableOutput("table_3")
                )
              )
+    )
     ), 
     
     tabPanel(
       h4("Bed Occupancy"), 
              
              fluidRow(
-               HTML("<br><br><br>"),
-               
+               column(width = 6, 
+
                HTML("<h3> <b> Yearly </b> </h3>"),
                
                sidebarLayout(
@@ -127,28 +137,34 @@ ui <- fluidPage(
                  )
                )
              ), 
-             fluidRow( 
-               
-               HTML("<br><br><br>"),
+             column(width =6,  
                
                HTML("<h3> <b> Seasonal </b> </h3>"),
                
+               column(width = 4, 
+               
                mainPanel(
                  radioButtons("period_1", "", choices = c("Pandemic" = "Pandemic","Prior to pandemic" = "Pre-pandemic")),
-                 radioButtons("m_2", "", choices = c("Percentage of available beds occupied" = "percentage_occupancy","Average number of beds occupied" = "average_occupied_beds")),
-                 plotOutput("season_b"),
+                 radioButtons("m_2", "", choices = c("Percentage of available beds occupied" = "percentage_occupancy","Average number of beds occupied" = "average_occupied_beds")))),
+                column(width = 8, plotOutput("season_b"),
                  dataTableOutput("table_b2")
                )
-             ),
+             )
+            ,
              
+            tags$br(), 
+            tags$hr(), 
+            tags$br(),
+            
              fluidRow(
                mainPanel(
                  plotOutput("graph_output_bed")
                )
              )
+      )
       ),
    
-     tabPanel("Map", 
+     tabPanel(h4("Map"), 
               fluidRow(
                 mainPanel(
                   leafletOutput("map_output_bed")
@@ -200,6 +216,7 @@ ui <- fluidPage(
       tags$br(),
       tags$hr(),
       tags$br(),
+      
       fluidRow(
         column(
           4,
